4fad63c681ca44b895ce6792b4e62b264b3f91f1
Nick DiRienzo <me@nickdirienzo.com>
2014-11-20 00:46:09 -0500

    because they hate me

--- ubot_helper.py
+++ ubot_helper.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def whois(name):
   if '@' in name:
     name = name.split('@')[-1]
+  name = name.lower()
   resp = requests.get('https://www.buffalo.edu/directory/user/%s.vcf' % name)
   return str(ub_vcf.Vcf(resp.text))
 
