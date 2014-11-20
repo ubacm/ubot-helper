d40f99c85b86024cb82376b88fc546198a4fdc54
Nick DiRienzo <me@nickdirienzo.com>
2014-11-20 00:23:46 -0500

    fixed url case

--- ub_vcf.py
+++ ub_vcf.py
@@ -5,17 +5,23 @@ class Vcf(object):
 
   def process(self):
     for line in self.vcf_lines:
-      key, value = line.split(':')
+      key, value = line.split(':', 1)
       if key == 'FN':
         self.name = value
       if key == 'ORG':
         self.uni, self.major = value.split(';')
       if key == 'TITLE':
         self.title = value
+      if key == 'URL':
+        self.website = value
+      if 'EMAIL' in key:
+        self.email = value
 
   def __str__(self):
     return '''
     Name: %s
     Major: %s
     Title: %s
-    ''' % (self.name, self.major, self.title)
+    Website: %s
+    Email: %s
+    ''' % (self.name, self.major, self.title, self.website, self.email)
